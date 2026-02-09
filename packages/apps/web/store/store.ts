320955f2c63e7b6f0f62a9837d08a1e772941a81
정휘학 <gnlgrkdl@danusys.com>
2026-02-09 14:49:54 +0900

    [2026.02.09 UPDATE] useModeStore() 1.store 타입 지정 수정

--- packages/apps/web/store/store.ts
+++ packages/apps/web/store/store.ts
@@ -1,11 +1,11 @@
-import { create } from 'zustand/react';
+import { create } from 'zustand';
 import { ModeStoreType } from '@/apps/web/store/store.type';
 import { persist } from 'zustand/middleware';
 
 // * =========================
 // * 웹사이트 다크모드 설정 스토어
 // * =========================
-export const useModeStore = create<ModeStoreType>(
+export const useModeStore = create<ModeStoreType>()(
   persist(
     (set) => ({
       darkMode: false,
