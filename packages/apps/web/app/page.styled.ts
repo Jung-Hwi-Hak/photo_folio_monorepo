aa3cd5374cacc89e893faa2d39d185a7e530ca11
정휘학 <gnlgrkdl@danusys.com>
2026-02-09 14:24:28 +0900

    [2026.02.09 DELETE] 1.styled component 파일 제거 (tailwindcss 로 변경함)

--- packages/apps/web/app/page.styled.ts
+++
@@ -1,5 +0,0 @@
-import styled, { DefaultTheme } from 'styled-components';
-
-export const PageContainer = styled.div`
-  background-color: ${({ theme }: DefaultTheme) => theme.colors.gray900};
-`;
--- packages/apps/web/components/common/body/body.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled, { DefaultTheme } from 'styled-components';
-
-export const PortoFolioWrap = styled.div`
-  display: grid;
-  grid-template-columns: ${({ theme }: DefaultTheme) => theme.flexCardRepeat.repeat1to4};
-  row-gap: 5rem;
-  column-gap: 1.5rem;
-  color: olivedrab;
-`;
